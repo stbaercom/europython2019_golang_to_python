48efce13112c4d2d9bd18e8b6adb32acdc0a1a79
Stefan Baerisch <stefan@stbaer.com>
2019-06-23 09:58:10 +0200

    Lvl2 Build Script Working

--- lvl2/go/cgo_build_lvl2.sh
+++ lvl2/go/cgo_build_lvl2.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 LVL=2
-NAME="cgo_lib/cgo_lib_lvl${LVL}"
+NAME="cgo_lib_lvl${LVL}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
 
-SRC="${DIR}/go/${NAME}.{a,h}"
+CGO="${DIR}/go/cgo_lib"
 
 DSTPY="${DIR}/py/cgo_lib"
 
@@ -14,15 +14,13 @@ DSTCPP="${DIR}/cpp/cgo_lib"
 
 #echo ${LVL} ${NAME} ${DIR} $'\n' ${SRCDIR} $'\n' ${DSTPY} $'\n' ${DSTCPP}
 
-rm "${DIR}/go/cgo_lib/*.{a,h}"
+rm ${CGO}/*.{a,h} >/dev/null 2>&1
 
-go build  -o ${NAME}.a -buildmode=c-archive src/com.stbaer/demo_cgo/main.go
+go build  -o "${CGO}/${NAME}.a" -buildmode=c-archive src/com.stbaer/demo_cgo/main.go
 
-rm ${DSTPY}/*.{a,h}
-cp ${SRC} ${DSTPY}
+rm ${DSTPY}/*.{a,h} >/dev/null 2>&1
+cp ${CGO}/*.{a,h} ${DSTPY}
 
-rm ${DSTCPP}/*.{a,h}
-cp ${SRC} ${DSTCPP}
+rm ${DSTCPP}/*.{a,h} >/dev/null 2>&1
+cp ${CGO}/*.{a,h} ${DSTCPP}
 
-
-rm ${SRC}
